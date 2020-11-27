9d95bc953be12bfdca7ca8828536b17dd8983dd1
Nicolas Bertin <nicolas.bertin@sleede.com>
2020-11-27 17:14:15 +0100

    mvce #2

--- app/reflexes/example_reflex.rb
+++ app/reflexes/example_reflex.rb
@@ -3,12 +3,14 @@
 class ExampleReflex < ApplicationReflex
   delegate :uuid, to: :connection
 
-  def test
-    puts "We're live!"
+  def open_modal
+    count = 0
+    morph "#modal-container", ApplicationController.render(partial: "home/modal", locals: {count: count})
   end
 
   def update_count
-    @count = 1
-    @message = "Success"
+    count = element.dataset[:count].to_i + 1
+    morph "#modal-container", ApplicationController.render(partial: "home/modal", locals: {count: count})
   end
+
 end
